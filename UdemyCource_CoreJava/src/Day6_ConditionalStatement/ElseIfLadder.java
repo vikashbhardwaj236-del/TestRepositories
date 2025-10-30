57a40270f94cc9d10b4517370e59148ff95b803d
vikas.bhardwaj <vikas.bhardwaj236@gmail.com>
2025-10-30 19:53:34 +0530

    First Commit

---
+++ UdemyCource_CoreJava/src/Day6_ConditionalStatement/ElseIfLadder.java
@@ -0,0 +1,30 @@
+package Day6_ConditionalStatement;
+
+public class ElseIfLadder {
+
+	public static void main(String[] args) {
+
+int number = 0 ;
+
+if (number>0)
+{
+System.out.println("Number iS Positive");	
+
+}
+else if (number <0)
+{
+System.out.println("Number is Negative");	
+}
+else if (number == 0)
+{
+System.out.println("Number is Zero");
+
+}
+else 
+{
+System.out.println("Invalid Entry");	
+}
+
+	}
+
+}
---
+++ UdemyCource_CoreJava/src/Day6_ConditionalStatement/IfConditions.java
@@ -0,0 +1,20 @@
+package Day6_ConditionalStatement;
+
+public class IfConditions {
+
+	public static void main(String[] args) {
+
+
+		int Marks = 25;
+		if (Marks >=33)
+		{
+			System.out.println("Student is PASS if he Has greater than or equal: "+ Marks + " Marks.");
+		}
+		else
+		{
+			System.out.println("Student is FAIL if he Has Less than: "+ Marks + " Marks.");	
+		}
+
+	}
+
+}
